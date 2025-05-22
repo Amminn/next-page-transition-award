f1e731ad5f6bfa00aa2f93031109cad99acb86f2
Amin <55767303+Amminn@users.noreply.github.com>
2025-05-23 00:49:22 +0100

    feat: final result - all working fine

--- src/app/components/Nav.js
+++ src/app/components/Nav.js
@@ -4,21 +4,96 @@ import Link from "next/link";
 import { useTransitionRouter } from "next-view-transitions";
 
 export default function Nav() {
-  const router = useTransition();
+  const router = useTransitionRouter();
+
+  function slideInOut() {
+    document.documentElement.animate(
+      [
+        {
+          opacity: 1,
+          transform: "translateY(0)",
+        },
+        {
+          opacity: 0.2,
+          transform: "translateY(-35%)",
+        },
+      ],
+      {
+        duration: 1500,
+        easing: "cubic-bezier(0.87, 0, 0.13, 1)",
+        fill: "forwards",
+        pseudoElement: "::view-transition-old(root)",
+      }
+    );
+
+    document.documentElement.animate(
+      [
+        {
+          clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)",
+        },
+        {
+          clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
+        },
+      ],
+      {
+        duration: 1500,
+        easing: "cubic-bezier(0.87, 0, 0.13, 1)",
+        fill: "forwards",
+        pseudoElement: "::view-transition-new(root)",
+      }
+    );
+  }
+
+  function handleLinkClick(e, path) {
+    e.preventDefault();
+    router.push(path, {
+      onTransitionReady: slideInOut,
+    });
+  }
 
   return (
     <nav className="nav">
       <div className="logo">
         <div className="link">
-          <a href="/"></a>
+          <a
+            onClick={(e) => {
+              e.preventDefault();
+              router.push("/", {
+                onTransitionReady: slideInOut,
+              });
+            }}
+            href="/"
+          >
+            Index
+          </a>
         </div>
       </div>
       <div className="links">
         <div className="link">
-          <Link href="/project">Projects</Link>
+          <a
+            onClick={(e) => {
+              e.preventDefault();
+              router.push("/project", {
+                onTransitionReady: slideInOut,
+              });
+            }}
+            href="/project"
+          >
+            Projects
+          </a>
         </div>
         <div className="link">
-          <Link href="/info">Info</Link>
+          <a
+            onClick={(e) => {
+              e.preventDefault();
+              router.push("/info", {
+                onTransitionReady: slideInOut,
+              });
+            }}
+            href="/info"
+          >
+            Info
+          </a>
         </div>
       </div>
     </nav>
